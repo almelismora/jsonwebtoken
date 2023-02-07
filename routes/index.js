24683dad01876ff5366dd86fe332e8b13c6d12c5
Elis Mora <almedinaelis.mora@gmail.com>
2023-02-07 11:02:55 +0100

    rutas login y protected simples creadas usando jwt

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@ const jwt = require('jsonwebtoken')
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next)) {
+router.get('/', (req, res, next) => {
   res.json({
     text: 'api works'
   })
-}
+})
 
-
+router.post( '/api/login', (req, res) => {
+  const user = {id: 3}
+  const token = jwt.sign({user}, 'my_secret_key')
+})
 
 
 module.exports = router;
